9c167f77ec76711c046c13dbbdf746927165373f
dhvogel <dhvogel2468@gmail.com>
2018-04-24 00:26:34 -0700

    add jumpbikes functions

--- routes/jumpbikes.js
+++ routes/jumpbikes.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const request = require('request');
+
 module.exports.getAllBikes = function() {
 
+	return request.get('https://app.socialbicycles.com/api/bikes.json')
+		.on('response', function(response) {
+			console.info(response);
+		});
 };
