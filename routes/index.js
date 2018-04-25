0077918c3c01962eac425c5dcfd25e76ebef2805
dhvogel <dhvogel2468@gmail.com>
2018-04-24 18:14:01 -0700

    map working, showing jumpbike locations

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,21 @@ const router = express.Router();
 
 /* GET home page. */
 //eslint-disable-next-line
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
 	res.render('index', { title: 'Express' });
+});
+
+router.get('/health', function(req, res) {
+	res.send('OK');
 });
 
 /* GET /bikes */
 //eslint-disable-next-line
-router.get('/jumpbikes', function(req, res, next) {
+router.get('/jumpbikes', function(req, res) {
 	const sobiClientToken = jumpbikes.getSoBiClientToken();
 
 	jumpbikes.getAllBikes(sobiClientToken, function(error, response, body) {
-		res.render('jumpbikes', {
-			returnedBikes: JSON.parse(body).items
-		});
+		res.send(JSON.parse(body).items);
 	});
 });
 
