99e8467c3f472f52c4b0e954c88bb0e6942472b0
dhvogel <dhvogel2468@gmail.com>
2018-04-25 17:53:00 -0700

    added integration tests for GET /jumpbikes

--- test/integration/jumpbikes.integration.tests.js
+++ test/integration/jumpbikes.integration.tests.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 const server = require('../../app');
 const chai = require('chai');
-const sinon = require('sinon');
 
 chai.should();
 
@@ -12,6 +11,35 @@ describe('Jumpbikes -- Integration Tests', function() {
 		request(server)
 			.get('/health')
 			.expect(200, done);
+
+	});
+
+	describe('GET /jumpbikes', function() {
+
+		it('responds to /jumpbikes', function(done) {
+
+			request(server)
+				.get('/jumpbikes')
+				.end(function(err, res) {
+					(res.statusCode).should.equal(200);
+					(res.body).should.be.an('array');
+
+					(res.body[0]).should.have.property('id').that.is.a('number');
+					(res.body[0]).should.have.property('name').that.is.a('string');
+					(res.body[0]).should.have.property('network_id').that.is.a('number');
+					(res.body[0]).should.have.property('id').that.is.a('number');
+					(res.body[0]).should.have.property('sponsored').that.is.a('boolean');
+					(res.body[0]).should.have.property('ebike_battery_level').that.is.a('number');
+					(res.body[0]).should.have.property('ebike_battery_distance').that.is.a('number');
+					(res.body[0]).should.have.property('hub_id');
+					(res.body[0]).should.have.property('inside_area').that.is.a('boolean');
+					(res.body[0]).should.have.property('address').that.is.a('string');
+					(res.body[0]).should.have.property('current_position').that.is.an('object');
+
+					done();
+				});
+
+		});
 
 	});
 
