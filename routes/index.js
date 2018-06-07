07c1f370f7268cb2470d0a76d30fdd2e3e04b129
dhvogel <dhvogel2468@gmail.com>
2018-06-07 11:32:28 -0700

    add test data files, coinbase /user passing integration tests

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@
 
 const express = require('express');
 const jumpbikes = require('./jumpbikes');
+const coinbase = require('./coinbase');
 const async = require('async');
 const router = express.Router();
+
+const CB_API_VERSION = '2018-03-22';
 
 router.get('/', function(req, res) {
 	res.render('index', { title: 'Express' });
 });
 
 router.get('/health', function(req, res) {
-	res.send('OK');
+	res.status(200).send('OK');
 });
 
 router.get('/jumpbikes', function(req, res) {
@@ -40,6 +43,25 @@ router.get('/jumpbikes/:jumpbikeId', function(req, res) {
 		}
 	], function(err, sobiClientToken) {
 		jumpbikes.getSpecificBike(sobiClientToken, req.params.jumpbikeId,
+			function(error, response, body) {
+				if (error) {
+					console.log(error);
+					res.send(501);
+				}
+				res.send(JSON.parse(body));
+			});
+	});
+});
+
+router.get('/coinbase/user', function(req, res) {
+	async.waterfall([
+		function(callback) {
+			coinbase.getCoinbaseClientToken(function(coinbaseClientToken) {
+				callback(null, coinbaseClientToken);
+			});
+		}
+	], function(err, coinbaseClientToken) {
+		coinbase.getUser(coinbaseClientToken, CB_API_VERSION,
 			function(error, response, body) {
 				if (error) {
 					console.log(error);
--- test/integration/coinbase.integration.tests.js
+++ test/integration/coinbase.integration.tests.js
@@ -3,6 +3,8 @@
 const supertest = require('supertest');
 const server = require('../../app');
 const chai = require('chai');
+const sinon = require('sinon');
+const request = require('request');
 
 chai.should();
 
@@ -16,4 +18,46 @@ describe('Coinbase -- Integration Tests', function() {
 
 	});
 
+	describe('GET /coinbase/user', function() {
+
+		let requestStub;
+
+		before(function() {
+			requestStub = sinon.stub(request, 'get');
+		});
+
+		after(function() {
+			request.get.restore();
+		});
+
+		it('responds to /coinbase/user', function(done) {
+
+			const responseObject = {
+				statusCode: 200,
+			};
+			const responseBody = require('../data/coinbase/coinbase.v2.user.json');
+
+			requestStub
+				.withArgs({
+					url: 'https://api.coinbase.com/v2/user',
+					headers: {
+						'Authorization': sinon.match.string,
+						'CB-VERSION': sinon.match.string
+					}
+				}, sinon.match.func)
+				.yields(null, responseObject, JSON.stringify(responseBody));
+
+
+			supertest(server)
+				.get('/coinbase/user')
+				.end(function(err, res) {
+					(res.statusCode).should.equal(200);
+
+					(res.body.id).should.equal('coinbase-id');
+					(res.body.name).should.equal('Daniel Vogel');
+
+					done();
+				});
+		});
+	});
 });
